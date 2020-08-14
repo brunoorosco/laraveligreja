0c6a54a05c30d351331b29374e990b497bb911cc
brunoorosco <brunoorosco@gmail.com>
2020-08-14 16:15:37 -0300

    admin lte

--- routes/web.php
+++ routes/web.php
@@ -17,57 +17,61 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group([
-    'middleware' => ['auth'],
-    'prefix' => 'admin',
-    'namespace' => 'Admin',
-    'name' => 'admin.'
-], function () {
-    Route::get('/', function () {
-        return 'admin';
-    })->name('dashboard');
-
-    Route::get('/dashboard', function () {
-        return 'dashboard';
-    })->name('dashboard');
-
-    Route::get('/relatorio', function () {
-        return 'relatorio';
-    })->name('relatorio');
+Route::get('/admin', function () {
+    return view('admin.dashboard');
 });
 
+// Route::group([
+//     'middleware' => ['auth'],
+//     'prefix' => 'admin',
+//     'namespace' => 'Admin',
+//     'name' => 'admin.'
+// ], function () {
+//     Route::get('/', function () {
+//         return 'admin';
+//     })->name('dashboard');
 
-Route::middleware([])->group(function () {
-    Route::get('/entrar', function () {
-        return 'login';
-    })->name('web.login');
+//     Route::get('/dashboard', function () {
+//         return 'dashboard';
+//     })->name('dashboard');
 
-    Route::get('/cadastrar', function () {
-        return 'cadastrar';
-    })->name('web.cadastrar');
+//     Route::get('/relatorio', function () {
+//         return 'relatorio';
+//     })->name('relatorio');
+// });
 
-    Route::get('/recuperar', function () {
-        return 'recuperar';
-    })->name('web.recuperar');
 
-    Route::get('/senha/{email}/{forget}', function () {
-        return 'senha';
-    })->name('web.senha');
+// Route::middleware([])->group(function () {
+//     Route::get('/entrar', function () {
+//         return 'login';
+//     })->name('web.login');
 
-    Route::post('/login', function () {
-        return 'login';
-    })->name('web.login');
+//     Route::get('/cadastrar', function () {
+//         return 'cadastrar';
+//     })->name('web.cadastrar');
 
-    Route::post('/register', function () {
-        return 'register';
-    })->name('web.register');
+//     Route::get('/recuperar', function () {
+//         return 'recuperar';
+//     })->name('web.recuperar');
 
-    Route::post('/forget', function () {
-        return 'forget';
-    })->name('web.forget');
+//     Route::get('/senha/{email}/{forget}', function () {
+//         return 'senha';
+//     })->name('web.senha');
 
-    Route::post('/reset', function () {
-        return 'reset';
-    })->name('web.reset');
-});
+//     Route::post('/login', function () {
+//         return 'login';
+//     })->name('web.login');
+
+//     Route::post('/register', function () {
+//         return 'register';
+//     })->name('web.register');
+
+//     Route::post('/forget', function () {
+//         return 'forget';
+//     })->name('web.forget');
+
+//     Route::post('/reset', function () {
+//         return 'reset';
+//     })->name('web.reset');
+// });
 
