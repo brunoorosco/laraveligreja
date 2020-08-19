5e92a99971ce85c5af2c1a936ed3b3101d53d226
brunoorosco <brunoorosco@gmail.com>
2020-08-18 22:04:28 -0300

    initial project

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/tes', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
+Route::get('/', function () {
     return view('admin.dashboard');
 });
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,27 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+//mix.js('resources/js/app.js', 'public/js')
+//  .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'resources/views/admin/assets/css/bootstrap.css',
+    'resources/views/admin/assets/css/now-ui-dashboard.css',
+    'resources/views/admin/assets/demo/demo.css',
+],
+    'public/admin/assets/css/style.css',
+);
+
+
+mix.js([
+    'resources/views/admin/assets/js/plugins/bootstrap-notify.js',
+    'resources/views/admin/assets/js/now-ui-dashboard.min.js?v=1.5.0',
+    'resources/views/admin/assets/js/core/bootstrap.min.js',
+    'resources/views/admin/assets/js/core/jquery.min.js',
+    'resources/views/admin/assets/js/core/popper.min.js', 
+    'resources/views/admin/assets/js/plugins/perfect-scrollbar.jquery.min.js',
+    'resources/views/admin/assets/js/plugins/chartjs.min.js',
+    'resources/views/admin/assets/demo/demo.js',
+],
+    'public/admin/assets/js/main.js',
+);
